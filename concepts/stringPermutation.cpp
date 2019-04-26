362246df909dd2b8dd4a3e2e6e2bac76bfd093b1
sirraghavgupta <raghav.mhc@gmail.com>
2019-04-26 04:22:46 -0400

    newer version of stringPermutation

--- concepts/stringPermutation.cpp
+++ concepts/stringPermutation.cpp
@@ -9,31 +9,42 @@ STATUS : !!! success !!!
 #include <cstring>
 using namespace std;
 
-// to count the number of occurences of all charcters in a string
+/* //to count the number of occurences of all charcters in a string
 void countChars(char string[], int frequency[]){
   for(int i=0; i<strlen(string); i++){
     int index= string[i]-'a';
     frequency[index]++;
   }
-}
+} */
 
 int main(){
 
 char string1[20], string2[20];
-int frequency1[26]={0}, frequency2[26]={0};    // for counting
+int frequency1[26]={0};    // for counting
 
 cin>>string1;
-// cout<<"u entered: "<<string1<<endl;
 cin>>string2;
-// cout<<"u entered: "<<string2<<endl;
 
-countChars(string1, frequency1);
-countChars(string2, frequency2);
+if(strlen(string1)!=strlen(string2)){   // length should be same for permutation
+  cout<<"not a permutation"<<endl;
+  return 0;
+}
 
+// prepare freq array for string 1
+for(int i=0; i<strlen(string1); i++){
+  int index= string1[i]-'a';
+  frequency1[index]++;
+}
 
-// match the 2 frequency arrays
+// for second string, go on decrementing the first freq array
+for(int i=0; i<strlen(string2); i++){
+  int index= string2[i]-'a';
+  frequency1[index]--;
+}
+
+// check if the freq array has become all 0
 for(int i=0; i<26; i++){
-  if(frequency1[i] != frequency2[i]){
+  if(frequency1[i]!=0){
     cout<<"not a permutation"<<endl;
     return 0;
   }
